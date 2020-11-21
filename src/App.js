211cdf524d05e458f1303e5d9dbbac4fc33f0450
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-11-21 11:54:40 -0500

    fixed deploy error

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Router>
           <Header />
           <Container>
-            <Route exact path="/" component={Employees} />
+          <Employees></Employees>
           </Container>
         </Router>
       </div>
